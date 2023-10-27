cd724d5c85eea6da8ca3ceb2887d77eb2f688b36
Chan-Min, Kim <kimchanmin1@gmail.com>
2023-10-27 23:04:29 +0900

    Delete src/Axios.js

--- src/Axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://localhost:5001/challenge-87510/us-central1/api/", // API (cloud function) URL
-});
-
-export default instance;
